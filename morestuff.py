fa590c4b510e0ca60eca3b49d0c7a3a906c7ee91
mctschaly <mctschaly@gmail.com>
2021-05-20 18:14:38 +0200

    some changes

--- morestuff.py
+++ morestuff.py
@@ -1,4 +1,5 @@
 def binom(n, k):
+    '''slowly calculate the binomial coefficient'''
     if n <= 0:
         return 1
     if n < 2 * k:
@@ -19,3 +20,18 @@ for n in range(11):
     for k in range(n + 1):
         print(binom(n, k), end="\t")
     print()
+
+
+def fibList(n):  # write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=" ")
+        a, b = b, a + b
+    print()
+
+
+fibList(10000000)
+
+def fib(n):
+
